57e6271f60aefd9651d873110ee2357f90e9de1c
ugurcoskn <ugurcoskun232@gmail.com>
2024-07-04 17:04:04 +0300

    feat: change css to module css

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,57 +1,57 @@
-import "./login.css";
+import styles from "./login.module.css";
 
 export default function pages() {
   return (
-    <div className="container">
-      <div className="left-panel">
-        <p className="welcome-text">Welcome,</p>
-        <p className="sign-up-text">Please sign up to login</p>
-        <button className="sign-up-button">Sign Up</button>
+    <div className={styles.container}>
+      <div className={styles["left-panel"]}>
+        <p className={styles["welcome-text"]}>Welcome,</p>
+        <p className={styles["sign-up-text"]}>Please sign up to login</p>
+        <button className={styles["sign-up-button"]}>Sign Up</button>
       </div>
-      <div className="right-panel">
-        <p className="login-text">Login</p>
-        <div className="person-name">
-          <label className="person-name">Enter your first name</label>
+      <div className={styles["right-panel"]}>
+        <p className={styles["login-text"]}>Login</p>
+        <div className={styles["person-name"]}>
+          <label className={styles["person-name"]}>Enter your first name</label>
           <input
             type="text"
             id="person-name-input"
             name="person-name"
-            className="person-name-input"
+            className={styles["person-name-input"]}
           />
         </div>
 
-        <div className="person-surname">
+        <div className={styles["person-surname"]}>
           <label className="person-surname">Enter your last name</label>
           <input
             type="text"
             id="person-surname"
             name="person-surname"
-            className="person-surname-input"
+            className={styles["person-surname-input"]}
           />
         </div>
 
-        <div className="person-email">
+        <div className={styles["person-email"]}>
           <label className="person-email">Enter your email</label>
           <input
             type="email"
             id="person-email"
             name="person-email"
-            className="person-email-input"
+            className={styles["person-email-input"]}
           />
         </div>
 
-        <div className="person-password">
+        <div className={styles["person-password"]}>
           <label className="person-password">Enter your password</label>
           <input
             type="password"
             id="person-password"
             name="person-password"
-            className="person-password-input"
+            className={styles["person-password-input"]}
           />
         </div>
 
         <div>
-          <button className="login-button">Login</button>
+          <button className={styles["login-button"]}>Login</button>
         </div>
       </div>
     </div>
