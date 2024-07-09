8b6cc288474c7a0f5d05a093e25f5cdf41782971
Begum <94627984+begumaksoy@users.noreply.github.com>
2024-07-09 11:34:21 +0300

    added uuid library for User interface id generation

--- src/app/actions/user.ts
+++ src/app/actions/user.ts
@@ -1,5 +1,7 @@
+import { v4 as uuidv4 } from 'uuid';
 
 interface User{
+    id: string;
     email: string;
     fullname: string;
     username: string;
@@ -10,7 +12,7 @@ let userArray: User[] = [];
 
 export async function saveUser(data: FormData) {
     "use server";
-    let user = {email : data.get("email"), fullname: data.get("fullname"), username: data.get("username"), password: data.get("password")} as User;
+    let user = {id: uuidv4(), email : data.get("email"), fullname: data.get("fullname"), username: data.get("username"), password: data.get("password")} as User;
     userArray.push(user);
     console.log(userArray);
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,37 +4,37 @@ import Link from "next/link";
 export default function pages() {
   return (
     <div className={styles.container}>
-      <div className={styles["left-panel"]}>
-        <p className={styles["welcome-text"]}>Welcome,</p>
-        <p className={styles["sign-up-text"]}>Please sign up to login</p>
+      <div className={styles.leftPanel}>
+        <p className={styles["welcomeText"]}>Welcome,</p>
+        <p className={styles.signUpText}>Please sign up to login</p>
         <Link href="/sign-up">
-          <button className={styles["sign-up-button"]}>Sign Up</button>
+          <button className={styles.signUpButton}>Sign Up</button>
         </Link>
       </div>
-      <div className={styles["right-panel"]}>
-        <p className={styles["login-text"]}>Login</p>
+      <div className={styles["rightPanel"]}>
+        <p className={styles["loginText"]}>Login</p>
         <div className={styles["person-email"]}>
           <label className="person-email">Enter your email or username</label>
           <input
             type="email"
             id="person-email"
             name="person-email"
-            className={styles["person-email-input"]}
+            className={styles["personEmailInput"]}
           />
         </div>
 
-        <div className={styles["person-password"]}>
+        <div className={styles["personPassword"]}>
           <label className="person-password">Enter your password</label>
           <input
             type="password"
             id="person-password"
             name="person-password"
-            className={styles["person-password-input"]}
+            className={styles["personPasswordInput"]}
           />
         </div>
 
         <div>
-          <button className={styles["login-button"]}>Login</button>
+          <button className={styles["loginButton"]}>Login</button>
         </div>
       </div>
     </div>
